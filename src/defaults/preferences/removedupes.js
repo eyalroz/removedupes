248325182cab4e2c842df9b8349e2df75efec36e
Eyal Rozenberg <eyalroz@technion.ac.il>
2007-07-06 22:21:41 +0000

    fix for bug 17245 (reworked algorithm) partial fix for bug 17260 (move to any folder)

--- src/defaults/preferences/removedupes.js
+++ src/defaults/preferences/removedupes.js
@@ -1,11 +1,13 @@
 // visible prefs
 pref("removedupes.mail.comparison_criteria.from", true);
+pref("removedupes.mail.comparison_criteria.folder", false);
 pref("removedupes.mail.comparison_criteria.message_id", true);
 pref("removedupes.mail.comparison_criteria.num_lines", false);
 pref("removedupes.mail.comparison_criteria.send_time", true);
 pref("removedupes.mail.comparison_criteria.subject", true);
 pref("removedupes.mail.search_subfolders_first", false);
 pref("removedupes.mail.confirm_search_and_deletion", true);
-pref("removedupes.mail.move_to_trash_by_default", true);
+pref("removedupes.mail.default_action", true);
 
 // hidden prefs
+
