db9ba7ff9fc9fa681ec966827b6b6f0603b53919
eyalroz <eyalroz>
2007-07-07 12:17:11 +0000

    minor remaining fixes for special folder expection issue, updated German locale

--- src/defaults/preferences/removedupes.js
+++ src/defaults/preferences/removedupes.js
@@ -7,7 +7,7 @@ pref("removedupes.mail.comparison_criteria.send_time", true);
 pref("removedupes.mail.comparison_criteria.subject", true);
 pref("removedupes.mail.search_subfolders_first", false);
 pref("removedupes.mail.confirm_search_and_deletion", true);
-pref("removedupes.mail.default_action", true);
+pref("removedupes.mail.default_action", 'move');
 
 // hidden prefs
 
