0509cc4504416bc01762e6cf7bfcf3fd7701ee91
Eyal Rozenberg <eyalroz@technion.ac.il>
2008-03-22 15:19:56 +0000

    fix for bug 18830 - removedupes.mail. prefs now removedupes. only

--- src/defaults/preferences/removedupes.js
+++ src/defaults/preferences/removedupes.js
@@ -1,21 +1,20 @@
 // visible prefs
 
-pref("removedupes.mail.comparison_criteria.message_id", true);
-pref("removedupes.mail.comparison_criteria.send_time", true);
-pref("removedupes.mail.comparison_criteria.folder", false);
-pref("removedupes.mail.comparison_criteria.subject", true);
-pref("removedupes.mail.comparison_criteria.author", true);
-pref("removedupes.mail.comparison_criteria.num_lines", false);
-pref("removedupes.mail.comparison_criteria.recipients", false);
-pref("removedupes.mail.comparison_criteria.cc_list", false);
-pref("removedupes.mail.comparison_criteria.flags", false);
-pref("removedupes.mail.comparison_criteria.body", false);
+pref("removedupes.comparison_criteria.message_id", true);
+pref("removedupes.comparison_criteria.send_time", true);
+pref("removedupes.comparison_criteria.folder", false);
+pref("removedupes.comparison_criteria.subject", true);
+pref("removedupes.comparison_criteria.author", true);
+pref("removedupes.comparison_criteria.num_lines", false);
+pref("removedupes.comparison_criteria.recipients", false);
+pref("removedupes.comparison_criteria.cc_list", false);
+pref("removedupes.comparison_criteria.flags", false);
+pref("removedupes.comparison_criteria.body", false);
 
-pref("removedupes.mail.confirm_search_and_deletion", true);
-pref("removedupes.mail.default_action", 'move');
+pref("removedupes.confirm_search_and_deletion", true);
+pref("removedupes.default_action", 'move');
 
 // hidden prefs
 
-pref("removedupes.mail.allowed_special_folders", "chrome://removedupes/locale/removedupes.properties");
-pref('removedupes.mail.default_target_folder', 'mailbox://nobody@Local%20Folders/Trash');
-
+pref("removedupes.allowed_special_folders", "chrome://removedupes/locale/removedupes.properties");
+pref('removedupes.default_target_folder', 'mailbox://nobody@Local%20Folders/Trash');
