683ba6b6005e65a40c267fb85db58ea45f2b34b0
eyalroz <eyalroz>
2013-09-18 21:26:32 +0000

    Fix for bug 22576: Added extension. prefix to all preferences (Note: No migration code though)

--- src/defaults/preferences/removedupes.js
+++ src/defaults/preferences/removedupes.js
@@ -1,36 +1,36 @@
 // visible prefs
 
-pref("removedupes.search_subfolders", true);
-pref("removedupes.skip_special_folders", true);
+pref("extensions.removedupes.search_subfolders", true);
+pref("extensions.removedupes.skip_special_folders", true);
 
-pref("removedupes.compare_stripped_and_sorted_addresses", false);
+pref("extensions.removedupes.compare_stripped_and_sorted_addresses", false);
 // possible values are: seconds, minutes, hours, day, month, year
-pref("removedupes.time_comparison_resolution", "seconds");
-pref("removedupes.comparison_criteria.allow_md5_id_substitute", false);
+pref("extensions.removedupes.time_comparison_resolution", "seconds");
+pref("extensions.removedupes.comparison_criteria.allow_md5_id_substitute", false);
 
-pref("removedupes.comparison_criteria.message_id", true);
-pref("removedupes.comparison_criteria.send_time", true);
-pref("removedupes.comparison_criteria.size", true);
-pref("removedupes.comparison_criteria.folder", false);
-pref("removedupes.comparison_criteria.subject", true);
-pref("removedupes.comparison_criteria.author", true);
-pref("removedupes.comparison_criteria.num_lines", false);
-pref("removedupes.comparison_criteria.recipients", false);
-pref("removedupes.comparison_criteria.cc_list", false);
-pref("removedupes.comparison_criteria.flags", false);
-pref("removedupes.comparison_criteria.body", false);
+pref("extensions.removedupes.comparison_criteria.message_id", true);
+pref("extensions.removedupes.comparison_criteria.send_time", true);
+pref("extensions.removedupes.comparison_criteria.size", true);
+pref("extensions.removedupes.comparison_criteria.folder", false);
+pref("extensions.removedupes.comparison_criteria.subject", true);
+pref("extensions.removedupes.comparison_criteria.author", true);
+pref("extensions.removedupes.comparison_criteria.num_lines", false);
+pref("extensions.removedupes.comparison_criteria.recipients", false);
+pref("extensions.removedupes.comparison_criteria.cc_list", false);
+pref("extensions.removedupes.comparison_criteria.flags", false);
+pref("extensions.removedupes.comparison_criteria.body", false);
 
-pref("removedupes.use_dialog_before_removal", true);
-pref("removedupes.confirm_permanent_deletion", true);
-pref("removedupes.default_action", 'move');
+pref("extensions.removedupes.use_dialog_before_removal", true);
+pref("extensions.removedupes.confirm_permanent_deletion", true);
+pref("extensions.removedupes.default_action", 'move');
 
-pref("removedupes.limit_number_of_processed_messages", false);
-pref("removedupes.processed_messages_limit", 10000);
+pref("extensions.removedupes.limit_number_of_processed_messages", false);
+pref("extensions.removedupes.processed_messages_limit", 10000);
 
 
 // hidden prefs
 
-// no default for 'removedupes.default_target_folder'
+// no default for 'extensions.removedupes.default_target_folder'
 
-pref('removedupes.status_report_quantum',500);
-pref('removedupes.yield_quantum',2500);
+pref('extensions.removedupes.status_report_quantum',500);
+pref('extensions.removedupes.yield_quantum',2500);
