3fbc6a788dea293a1b650cc71dc11435cbb04ef3
eyalroz <eyalroz>
2019-10-06 21:10:41 +0000

    Work towards Thunderbird 68 compatibility:
    * Bumped version to 0.4
    * With newer app versions, overlaying the messenger directly rather than its
    overlay
    * Building a `manifest.json` file within the XPI
    * Miscelaneous dobuild tweaks (not version-68 specific)
    * Creating a link to the XPI without the version number (not
    version-68-related)
    * maxVersion bump to 68... in the `install.rdf` :-(
    * Additional variants of some files created using `jar.mn` and some XUL
    preprocessing: `dialog.xul`, `prefs.xul`
    * Naming changes in `RemoveDupes.App` (not really necessary for TB 68)
    * Now robustly importing the `removedues-common.js` module - so that importation
    succeeds on older TB versions as well (without `ChromeUtils.import`); also, never
    including the `removedupes-common.js` directly in XULs.
    * Typo correction in removedues-common.js exported symbol
    * Added: `setAppStringPref()` function (to wrap some pre/post TB-58 differences)
    * Adapted `removedupes-prefs.js` and `removedupes-dialog.js` to work with both
    pre-TB-68 and post-TB-68 versions of the XUL, particualrly with changes to
    elements and to elements' interfaces
    * Renamed: `XBL_FOLDER_PICKER` -> `XBL_FOLDER_PICKER_OR_REPLACEMENT` since with TB
    68 the folder picker is not XBL (yet behaves like the XBL folder picker)
    * Added: A bunch of stylesheet inclusions to `dialog.xul` and `prefs.xul` - to
    accommodate locations of styling code in TB 68.

---
+++ src/chrome/content/removedupes/removedupes-set-preference-types.js
@@ -0,0 +1,23 @@
+// Note: This code is only relevant for Thunderbird versions 68 and later,
+// where we can no longer use the XUL Preference element.
+
+Preferences.addAll([
+  { id: "extensions.removedupes.skip_special_folders",                   type: "bool"   },
+  { id: "extensions.removedupes.skip_imap_deleted_messages",             type: "bool"   },
+  { id: "extensions.removedupes.search_subfolders",                      type: "bool"   },
+  { id: "extensions.removedupes.use_dialog_before_removal",              type: "bool"   },
+  { id: "extensions.removedupes.default_action",                         type: "string" },
+  { id: "extensions.removedupes.compare_stripped_and_sorted_addresses",  type: "bool"   },
+  { id: "extensions.removedupes.comparison_criteria.author",             type: "bool"   },
+  { id: "extensions.removedupes.comparison_criteria.recipients",         type: "bool"   },
+  { id: "extensions.removedupes.comparison_criteria.cc_list",            type: "bool"   },
+  { id: "extensions.removedupes.comparison_criteria.message_id",         type: "bool"   },
+  { id: "extensions.removedupes.comparison_criteria.num_lines",          type: "bool"   },
+  { id: "extensions.removedupes.comparison_criteria.send_time",          type: "bool"   },
+  { id: "extensions.removedupes.comparison_criteria.size",               type: "bool"   },
+  { id: "extensions.removedupes.comparison_criteria.subject",            type: "bool"   },
+  { id: "extensions.removedupes.comparison_criteria.folder",             type: "bool"   },
+  { id: "extensions.removedupes.comparison_criteria.body",               type: "bool"   },
+  { id: "extensions.removedupes.time_comparison_resolution",             type: "string" },
+]);
+
