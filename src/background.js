1496fddbfb09ab78658bc3d9359606f3134ddac9
Eyal Rozenberg <eyalroz1@gmx.com>
2024-07-31 01:49:25 +0300

    Loader script (`background.js`) generification phases (3.) and (4.):
    * Loader script now loaded via an `HTML` file (`background.html`) <- Do we really need this?
    * Tabular data now specified in `manifest.json`, processed, then passed on for registration
    * Loader script now completely unaware of the extension it's being run for!

--- src/background.js
+++ src/background.js
@@ -1,84 +1,69 @@
-// Loader / background script
-// for the removedupes Thunderbird extension
-// by Eyal Rozenberg
+// Thunderbird regular extension loader script
+//
+// Note: All extension-specific information is passed through the manifest; avoid
+// modifying this file unless absolutely necessary
 
-function registerDefaultPrefs(manifest) {
+function registerChromeUrls(locales) {
+  let manifest = browser.runtime.getManifest();
   let shortname = manifest.short_name;
-  if (!shortname && !manifest.default_prefs) {
-    return;
+  locales ??= manifest.locales;
+  if (!shortname || !locales) {
+    return false;
   }
-  let path = manifest.default_prefs ?? `defaults/preferences/${shortname}.js`;
-  messenger.WindowListener.registerDefaultPrefs(path);
+  // I know this is weird, I don't understand this ja-JP-mac business myself; do we need it?
+  let localeToPath = (localeSpec) => `chrome/locale/${(localeSpec == "ja-JP-mac") ? "ja" : localeSpec}/`;
+  let localeToChromeUrl = (localeSpec) => ["locale", shortname, localeSpec, localeToPath(localeSpec)];
+  let chromeUrls = [
+    ["content", shortname, "chrome/content/"],
+    ...locales.map(localeToChromeUrl)
+  ];
+  messenger.WindowListener.registerChromeUrl(chromeUrls);
+  return true;
 }
 
-function registerOptionsPage(manifest) {
+function registerDefaultPrefs(path) {
+  let manifest = browser.runtime.getManifest();
   let shortname = manifest.short_name;
-  if (!shortname && !manifest.options_dialog) {
-    return;
+  if (!(path || shortname || manifest.default_prefs)) {
+    return false;
   }
-  let uri = manifest.options_dialog ?? `chrome://${shortname}/content/${shortname}-prefs.xhtml`;
+  path ??= manifest.default_prefs ?? `defaults/preferences/${shortname}.js`;
+  messenger.WindowListener.registerDefaultPrefs(path);
+  return true;
+}
+
+function registerOptionsPage(uri) {
+  let manifest = browser.runtime.getManifest();
+  let shortname = manifest.short_name;
+  if (!(uri || shortname || manifest.options_dialog)) {
+    return false;
+  }
+  uri ??= manifest.options_dialog ?? `chrome://${shortname}/content/${shortname}-prefs.xhtml`;
   messenger.WindowListener.registerOptionsPage(uri);
+  return true;
 }
 
 // TODO: Instead of registering overlay injectors for windows,
 // we should generate overlay injectors ourselves
-function registerChromeInjectors(manifest, registrationInfo) {
+function registerChromeInjectors(chromeInjectors) {
+  let manifest = browser.runtime.getManifest();
   let shortname = manifest.short_name;
   if (!shortname) {
-    return;
+    return false;
   }
-  for (let [windowHref, relativeInjectorPath] of registrationInfo) {
+  chromeInjectors ??= manifest.chrome_injectors;
+  for (let [windowHref, rawInjectionUri] of chromeInjectors) {
     let absoluteWindowHref = windowHref.startsWith('about:') ? windowHref : `chrome://messenger/content/${windowHref}`;
-    let jsFile = `chrome://${shortname}/content/overlay-injectors/${relativeInjectorPath}`;
-    messenger.WindowListener.registerWindow(absoluteWindowHref, jsFile);
+    let injectionUri = rawInjectionUri.startsWith(`chrome://`) ? rawInjectionUri : `chrome://${shortname}/content/overlay-injectors/${rawInjectionUri}`;
+    messenger.WindowListener.registerWindow(absoluteWindowHref, injectionUri);
   }
+  return true;
 }
 
 (async function () {
-  let manifest = browser.runtime.getManifest();
-  let shortname = manifest.short_name;
-  if (!manifest.short_name) { return; }
-
-  // Arrange it so that the register doesn't need us to repeat the shortname all the time
-  messenger.WindowListener.registerChromeUrl([
-    ["content", shortname,                                          "chrome/content/"],
-
-    // skin elements are no longer supported as such, they are now considered just part of the content
-    // Overlays can't just be registered and then apply; rather, we need to register an injector script, see below
-
-    // Style elements need not be registered; it's enough to just inject them later on (in injector scripts)
-
-    ["locale",  shortname, "en-US",                                 "chrome/locale/en-US/"],
-    ["locale",  shortname, "de",                                    "chrome/locale/de/"],
-    ["locale",  shortname, "it",                                    "chrome/locale/it/"],
-    ["locale",  shortname, "ja",                                    "chrome/locale/ja/"],
-    ["locale",  shortname, "ja-JP-mac",                             "chrome/locale/ja/"],
-    ["locale",  shortname, "zh-TW",                                 "chrome/locale/zh-TW/"],
-    ["locale",  shortname, "zh-CN",                                 "chrome/locale/zh-CN/"],
-    ["locale",  shortname, "sk-SK",                                 "chrome/locale/sk-SK/"],
-    ["locale",  shortname, "pt-PT",                                 "chrome/locale/pt-PT/"],
-    ["locale",  shortname, "pt-BR",                                 "chrome/locale/pt-BR/"],
-    ["locale",  shortname, "nl",                                    "chrome/locale/nl/"],
-    ["locale",  shortname, "fr",                                    "chrome/locale/fr/"],
-    ["locale",  shortname, "pl",                                    "chrome/locale/pl/"],
-    ["locale",  shortname, "he-IL",                                 "chrome/locale/he-IL/"],
-    ["locale",  shortname, "ru-RU",                                 "chrome/locale/ru-RU/"],
-    ["locale",  shortname, "da",                                    "chrome/locale/da/"],
-    ["locale",  shortname, "cs-CZ",                                 "chrome/locale/cs-CZ/"],
-    ["locale",  shortname, "es-AR",                                 "chrome/locale/es-AR/"],
-    ["locale",  shortname, "es-ES",                                 "chrome/locale/es-ES/"],
-    ["locale",  shortname, "is-IS",                                 "chrome/locale/is-IS/"],
-    ["locale",  shortname, "sv-SE",                                 "chrome/locale/sv-SE/"],
-    ["locale",  shortname, "sl-SI",                                 "chrome/locale/sl-SI/"]
-  ]);
-
-  registerChromeInjectors(manifest, [
-    ["messenger.xhtml",        "messenger.js"],
-    ["about:3pane",            "3pane.js"],
-    ["customizeToolbar.xhtml", "customizeToolbar.js"]
-  ]);
-
-  registerDefaultPrefs(manifest);
-  registerOptionsPage(manifest);
+  registerChromeUrls();
+  registerChromeInjectors();
+  registerDefaultPrefs();
+  registerOptionsPage();
   messenger.WindowListener.startListening();
 })();
